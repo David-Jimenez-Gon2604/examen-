e4d524f6136c4c9c33a9bacb33fc61e9662e47b9
David-Jimenez-Gon2604 <20253rd001@utez.edu.mx>
2026-03-06 12:58:10 -0600

    hacemos los setters y getters

--- examen/Reino_Magico.java
+++ examen/Reino_Magico.java
@@ -1,15 +1,16 @@
 public class Reino_Magico{
+
      private String nombre;
     private int edad;
     private String raza;
     private int nivel;
 
-    //contructor vacio
+    //vacio
     public static int totalPersonajes = 0;
     public Personaje() {
     }
 
-    // Constructor parametros
+    // parametros
     public Personaje(String nombre, int edad, String raza, int nivel) {
         this.nombre = nombre;
         this.edad = edad;
@@ -24,6 +25,47 @@ public class Reino_Magico{
         System.out.println("Nivel: " +this.nivel);
     }
 
+     public void saludar() {
+        System.out.println("El personaje " +this.nombre + " saluda al reino magico");
+    }
+    public void subirNivel(){
+        this.nivel++;
 
+    }
+    public void entrenar(){
+        this.nivel+=2;
+    }
     
+     // Getters y Setters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getRaza() {
+        return raza;
+    }
+
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
+
+    public int getNivel() {
+        return nivel;
+    }
+
+    public void setNivel(int nivel) {
+        this.nivel = nivel;
+    }
 }
