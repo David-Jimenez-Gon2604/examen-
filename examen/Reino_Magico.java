42f6fdb27dc69d174a8a4a7649572d0963433981
David-Jimenez-Gon2604 <20253rd001@utez.edu.mx>
2026-03-06 12:36:52 -0600

    creamos las variables declarando su nombre, edad, raza, y nivel

--- examen/Reino_Magico.java
+++ examen/Reino_Magico.java
@@ -1,3 +1,23 @@
 public class Reino_Magico{
+     private String nombre;
+    private int edad;
+    private String raza;
+    private int nivel;
+
+    //contructor vacio
+    public static int totalPersonajes = 0;
+    public Personaje() {
+        totalPersonajes
+    }
+
+    // Constructor parametros
+    public Personaje(String nombre, int edad, String raza, int nivel) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.raza = raza;
+        this.nivel = nivel;
+        
+    }
+
     
 }
